1e844de47b8a65151a6e116f4edeb80fc9fbfc1b
junseon <114742887+cow-98@users.noreply.github.com>
2024-03-28 00:10:49 +0900

    binding

--- FastCampusMetric/app/src/main/java/com/example/fastcampusmetric/MainActivity.kt
+++ FastCampusMetric/app/src/main/java/com/example/fastcampusmetric/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.fastcampusmetric
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fastcampusmetric.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
     }
+
 }
