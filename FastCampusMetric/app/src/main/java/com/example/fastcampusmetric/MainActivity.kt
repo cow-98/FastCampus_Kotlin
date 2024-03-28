14428999ed4e5fe07b82b3c5fedeff8b5e09e9f2
junseon <114742887+cow-98@users.noreply.github.com>
2024-03-29 00:28:53 +0900

    fastcampus

--- FastCampusMetric/app/src/main/java/com/example/fastcampusmetric/MainActivity.kt
+++ FastCampusMetric/app/src/main/java/com/example/fastcampusmetric/MainActivity.kt
@@ -2,14 +2,53 @@ package com.example.fastcampusmetric
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
 import com.example.fastcampusmetric.databinding.ActivityMainBinding
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val mainEdt = binding.mainEdt
+        val mainText = binding.mainText
+        val cm = binding.cm
+        val m = binding.m
+        val btn = binding.mainBtn
+
+        var inputNumber : Int = 0
+        var change : Boolean = true
+
+        mainEdt.addTextChangedListener {text ->
+            inputNumber = if(text.isNullOrEmpty()) {
+                0
+            } else{
+                text.toString().toInt()
+            }
+
+            if (change){
+                mainText.text = inputNumber.times(0.01).toString()
+            } else{
+                mainText.text = inputNumber.times(100).toString()
+            }
+
+        }
+
+        btn.setOnClickListener{
+            change = !change
+            if(change){
+                m.text = "cm"
+                cm.text = "m"
+                mainText.text = inputNumber.times(0.01).toString()
+
+            } else {
+                m.text = "m"
+                cm.text = "cm"
+                mainText.text = inputNumber.times(100).toString()
+            }
+        }
     }
-
 }
