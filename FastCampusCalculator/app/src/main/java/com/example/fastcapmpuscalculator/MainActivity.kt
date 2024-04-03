dfd9bf7d9e14607b670a1a6751824a5f40598b97
junseon <114742887+cow-98@users.noreply.github.com>
2024-04-03 17:09:02 +0900

    onClick

--- FastCampusCalculator/app/src/main/java/com/example/fastcapmpuscalculator/MainActivity.kt
+++ FastCampusCalculator/app/src/main/java/com/example/fastcapmpuscalculator/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.fastcapmpuscalculator
 
 import android.os.Bundle
+import android.telephony.AccessNetworkConstants.AccessNetworkType
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.fastcapmpuscalculator.databinding.ActivityMainBinding
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val firstNumberText = StringBuilder("")
+    private val secondNumberText = StringBuilder("")
+    private val operatorText = StringBuilder("")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+       fun numberClicked(view: View) {
+
+       }
     }
 }
